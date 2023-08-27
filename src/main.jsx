705c44d0228430f18cd3741be25a7a15d876f387
dayday1222 <dayday1222@gmail.com>
2023-08-27 21:47:56 +0800

    new

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import ItemList from './ItemList.jsx'
 
-<link rel="stylesheet" href="./NewCss.css" />
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
